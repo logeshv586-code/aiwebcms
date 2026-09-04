0e248ba2bf84029341d7b27e55ddf063d1edb8df
logesh <logeshv586@gmail.com>
2026-09-04 13:17:51 +0530

    feat: load visual custom page builder styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 import './design-studio.css';
+import './page-builder.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>
